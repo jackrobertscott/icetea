d6b9e8f52413105f818b8e913f99c7661555be42
Jack Scott <mast.j.scott@gmail.com>
2018-11-26 14:07:10 +0800

    added testing scripts

---
+++ packages/lumbridge-theme/src/tests/index.ts
@@ -0,0 +1,9 @@
+import hello from '../utils/hello';
+import { expect } from 'chai';
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello World!');
+  });
+});
