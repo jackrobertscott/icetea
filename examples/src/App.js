df8f3201046098093ceb6e2a85ff424c985a6255
Jack Scott <mast.j.scott@gmail.com>
2018-11-29 17:55:58 +0800

    added a timer onto the auth instance example

--- examples/src/App.js
+++ examples/src/App.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'lumbridge-router';
 import Main from './layouts/Main';
 import mainRouter from './routes/mainRouter';
-import { setAuthInstance } from './services/auth';
 
 const Routes = mainRouter.setup();
 
 export default class App extends Component {
-  componentDidMount() {
-    setAuthInstance.watch({
-      data: data => console.log('watch', data),
-    });
-  }
-
   render() {
     return (
       <Main>
--- examples/src/FAQ.js
+++ examples/src/FAQ.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import { setAuthInstance } from './services/auth';
 
 export default class FAQ extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      time: null,
+    };
+  }
+
   componentDidMount() {
-    setAuthInstance.execute({ data: 'blahblahblah' });
+    this.interval = setInterval(() => {
+      setAuthInstance.execute({ data: { time: Date.now() } });
+    }, 1000);
+    this.unwatch = setAuthInstance.watch({
+      data: ({ data: { time } }) => this.setState({ time }),
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    if (this.unwatch) {
+      this.unwatch();
+    }
   }
 
   render() {
-    return <div>FAQ Page!</div>;
+    return <div>FAQ Page! Time: {this.state.time}</div>;
   }
 }
